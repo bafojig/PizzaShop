ce1c640fb13854fbb4bef79e18fb3f0b970c6283
Will Patterson <wpatter3@students.kennesaw.edu>
2020-12-02 21:47:22 -0500

    sdfg

--- PizzaShop/Drinks.cs
+++ PizzaShop/Drinks.cs
@@ -31,7 +31,7 @@ namespace PizzaShop
         {
             List<string> drinks = new List<string>(); 
             Item i = new Item();
-            foreach (string s in drinkBox.CheckedItems +)
+            foreach (string s in drinkBox.CheckedItems)
             {
                 i.name = s;
                 i.price = 2.00;
